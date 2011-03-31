0c9b4c8a4b58c67f8edba123bac51ec3eb57f1f8
plaboy <plakitboy@gmail.com>
2011-03-31 20:50:14 +0800

    rename weBlog to blog

--- urls.py
+++ urls.py
@@ -2,7 +2,7 @@ from django.conf.urls.defaults import *
 
 import os.path
 
-from weBlog.views import Entry
+from blog.views import Entry
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -25,17 +25,17 @@ urlpatterns = patterns('',
         
     # Uncomment the next line to enable the admin:
     (r'^admin/', include(admin.site.urls)),
-    (r'^tiny_mce/(?P<path>.*)$', 'django.views.static.serve', {'document_root':os.path.join(os.path.dirname(__file__),'tiny_mce').replace('\\','/')}),
+    (r'^tiny_mce/(?P<path>.*)$', 'django.views.static.serve', {'document_root':os.path.join(os.path.dirname(__file__),'templates/tiny_mce').replace('\\','/')}),
     #(r'^tiny_mce/(?P<path>.*)$', 'django.views.static.serve', {'document_root': '/home/realjld/tiny/mysite/tinyCMS/tiny_mce'}),
     (r'^search/$', 'search.views.search'),
 
     # weBlog
-    (r'^weblog/$', 'django.views.generic.date_based.archive_index', entry_info_dict),
-    (r'^weblog/(?P<year>\d{4})/$', 'django.views.generic.date_based.archive_year', entry_info_dict),
-    (r'^weblog/(?P<year>\d{4})/(?P<month>\w{3})/$', 'django.views.generic.date_based.archive_month', entry_info_dict),
-    (r'^weblog/(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{2})/$', 'django.views.generic.date_based.archive_day', entry_info_dict),
+    (r'^blog/$', 'django.views.generic.date_based.archive_index', entry_info_dict),
+    (r'^blog/(?P<year>\d{4})/$', 'django.views.generic.date_based.archive_year', entry_info_dict),
+    (r'^blog/(?P<year>\d{4})/(?P<month>\w{3})/$', 'django.views.generic.date_based.archive_month', entry_info_dict),
+    (r'^blog/(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{2})/$', 'django.views.generic.date_based.archive_day', entry_info_dict),
     # (r'^weblog/(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{2})/(?P<slug>[-\w]+)/$', 'weBlog.views.entry_detail'),
-    (r'^weblog/(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{2})/(?P<slug>[-\w]+)/$',
+    (r'^blog/(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{2})/(?P<slug>[-\w]+)/$',
             'django.views.generic.date_based.object_detail', entry_info_dict),
 
 
