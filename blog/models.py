14a9b4b9b19022b89aa877832cd6d17b37ff5d3f
plaboy <plakitboy@gmail.com>
2011-04-01 17:44:39 +0800

    @models.permalink

--- blog/models.py
+++ blog/models.py
@@ -59,11 +59,12 @@ class Entry(models.Model):
     def __unicode__(self):
         return self.title
 
+    @models.permalink
     def get_absolute_url(self):
         return ('blog_entry_detail',(),{ 'year': self.pub_date.strftime("%Y"),
                                          'month': self.pub_date.strftime("%b").lower(),
                                          'day': self.pub_date.strftime("%d"),
-                                         'slug': se;f.slug })
-    get_absulute_url = models.permalink(get_absolute_url)
+                                         'slug': self.slug })
+    #get_absolute_url = models.permalink(get_absolute_url)
 
 
