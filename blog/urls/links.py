326713cf69b702eb315d9f0a0b689fe9bb663a99
tiny <plakitboy@gmail.com>
2011-05-19 23:26:43 +0800

    word links not needed any more in urls

--- blog/urls/links.py
+++ blog/urls/links.py
@@ -7,10 +7,10 @@ link_info_dict ={
 }
 
 urlpatterns = patterns('django.views.generic.date_based',
-                       (r'^links/$', 'archive_index', link_info_dict, 'blog_link_archive_index'),
-                       (r'^links/(?P<year>\d{4})/$', 'archive_year', link_info_dict, 'blog_link_archive_year'),
-                       (r'^links/(?P<year>\d{4})/(?P<month>\w{3})/$', 'archive_month', link_info_dict, 'blog_link_archive_month'),
-                       (r'^links/(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{2})/$', 'archive_day', link_info_dict, 'blog_link_archive_day'),
-                       (r'^links/(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{2#})/(?P<slug>[-\w]+)/$', 'object_detail', link_info_dict, 'blog_link_detail'),
+                       (r'^$', 'archive_index', link_info_dict, 'blog_link_archive_index'),
+                       (r'^(?P<year>\d{4})/$', 'archive_year', link_info_dict, 'blog_link_archive_year'),
+                       (r'^(?P<year>\d{4})/(?P<month>\w{3})/$', 'archive_month', link_info_dict, 'blog_link_archive_month'),
+                       (r'^(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{2})/$', 'archive_day', link_info_dict, 'blog_link_archive_day'),
+                       (r'^(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{2#})/(?P<slug>[-\w]+)/$', 'object_detail', link_info_dict, 'blog_link_detail'),
                       )
 
