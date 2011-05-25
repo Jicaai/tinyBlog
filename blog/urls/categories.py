8d2b59919c6bb9542661051018ce30ba70319522
Tiny <plakitboy@gmail.com>
2011-05-25 16:57:11 +0800

    pay attention to url's NAME define

--- blog/urls/categories.py
+++ blog/urls/categories.py
@@ -4,8 +4,8 @@ from blog.models import Category
 urlpatterns = patterns('',
                         (r'^$',
                          'django.views.generic.list_detail.object_list',
-                         {'queryset':Category.objects.all()}),
+                         {'queryset':Category.objects.all()}, 'blog_category_list'),
                         
                         (r'^(?P<slug>[-\w]+)/$', 
-                         'blog.views.category_detail'),
+                         'blog.views.category_detail','','blog_category_detail'),
                        )
