43cd0286de03fbfe352e8d1a09a68874858b64cc
Tiny <plakitboy@gmail.com>
2011-05-25 15:12:54 +0800

    del unnecessary line

--- urls.py
+++ urls.py
@@ -1,24 +1,12 @@
 from django.conf.urls.defaults import *
+from django.contrib import admin
 import os.path
 
-# Uncomment the next two lines to enable the admin:
-from django.contrib import admin
 admin.autodiscover()
 
-
 urlpatterns = patterns('',
-    # Example:
-    # (r'^tinyCMS/', include('tinyCMS.foo.urls')),
-
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
-    # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-        
-    # Uncomment the next line to enable the admin:
     (r'^admin/', include(admin.site.urls)),
     (r'^tiny_mce/(?P<path>.*)$', 'django.views.static.serve', {'document_root':os.path.join(os.path.dirname(__file__),'templates/tiny_mce').replace('\\','/')}),
-    #(r'^tiny_mce/(?P<path>.*)$', 'django.views.static.serve', {'document_root': '/home/realjld/tiny/mysite/tinyCMS/tiny_mce'}),
     (r'^search/$', 'search.views.search'),
 
     # weBlog
@@ -26,7 +14,6 @@ urlpatterns = patterns('',
     (r'^blog/links/', include('blog.urls.links')),
     (r'^blog/tags/', include('blog.urls.tags')),
     (r'^blog/', include('blog.urls.entries')),
-    #(r'^blog/', include('blog.urls')),
     #(r'', include('django.contrib.flatpages.urls')),
 )
 
