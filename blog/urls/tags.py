20659a7f631eccae81983686fa1c9f4d46e57933
ZhangJicai <plakitboy@gmail.com>
2011-05-29 20:58:44 +0800

    modify errors, add name for each url

--- blog/urls/tags.py
+++ blog/urls/tags.py
@@ -2,16 +2,16 @@ from django.conf.urls.defaults import *
 from tagging.models import Tag
 from blog.models import Entry, Link
 urlpatterns = patterns('',
-                        (r'^tags/$',
+                        (r'^$',
                          'django.views.generic.list_detail.object_list',
-                         {'queryset': Tag.objects.all()}),
+                         {'queryset': Tag.objects.all()}, 'blog_tag_list'),
                         
-                        (r'^tags/entries/(?P<tag>[-\w]+)/$',
+                        (r'^entries/(?P<tag>[-\w]+)/$',
                          'tagging.views.tagged_object_list',
                          {'queryset_or_model': Entry,
-                          'template_name': 'blog/entries_by_tag.html'}),
+                          'template_name': 'blog/entries_by_tag.html'}, 'entries_by_tag'),
 
-                        (r'^tags/entries/(?P<tag>[-\w]+)/$',
+                        (r'^entries/(?P<tag>[-\w]+)/$',
                          'tagging.views.tagged_object_list',
                          {'queryset_or_model': Link,
                           'template_name': 'blog/links_by_tag.html'}),
