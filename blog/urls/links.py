313bb5d540f0caf8c51b484bcc27499942265857
ZhangJicai <plakitboy@gmail.com>
2011-05-29 20:49:33 +0800

    modify errors:remove "link" in head of each url

--- blog/urls/links.py
+++ blog/urls/links.py
@@ -11,6 +11,6 @@ urlpatterns = patterns('django.views.generic.date_based',
                        (r'^(?P<year>\d{4})/$', 'archive_year', link_info_dict, 'blog_link_archive_year'),
                        (r'^(?P<year>\d{4})/(?P<month>\w{3})/$', 'archive_month', link_info_dict, 'blog_link_archive_month'),
                        (r'^(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{2})/$', 'archive_day', link_info_dict, 'blog_link_archive_day'),
-                       (r'^(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{2#})/(?P<slug>[-\w]+)/$', 'object_detail', link_info_dict, 'blog_link_detail'),
+                       (r'^(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{2})/(?P<slug>[-\w]+)/$', 'object_detail', link_info_dict, 'blog_link_detail'),
                       )
 
